64871d16cb442d22712809e80bbc46ffa44dce68
vegacantaayakkabionline-tech <vegacantaayakkabionline@gmail.com>
2026-05-11 21:43:16 +0300

    env variables

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://hgymlrwirhnygtmskeij.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhneW1scndpcmhueWd0bXNrZWlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0MDgxNzAsImV4cCI6MjA5Mzk4NDE3MH0.J3iatlrGLGSoZIxyTjYF1rez2A2nOPMeCMkUD8z_eF8'
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL
+const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
